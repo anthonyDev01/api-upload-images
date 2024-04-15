fde93a4de4db6440f4a539517ff7df58a835fe84
ANTHONY <anthonychukwudiDev@outlook.com>
2024-04-15 02:21:21 -0300

    subindo projeto

---
+++ src/test/java/com/api/uploadimages/UploadImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.uploadimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
